6c290c9c551e67e1d77a7102bf2521b010145950
hamoon-n1 <nikoo.hamoon@gmail.com>
2022-03-17 09:32:49 +1100

    rmi client factory

--- src/main/java/com/appdynamics/extensions/jmx/commons/JMXConnectionAdapter.java
+++ src/main/java/com/appdynamics/extensions/jmx/commons/JMXConnectionAdapter.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.rmi.ssl.SslRMIClientSocketFactory;
 
 public class JMXConnectionAdapter {
 
